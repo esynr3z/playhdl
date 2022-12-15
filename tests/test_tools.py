ce26f502e5040ad4ec7243f5c046b99784b06f4f
esynr3z <esynr3z@gmail.com>
2022-12-15 07:47:28 +0300

    feat: add tests for tools module

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -34,3 +34,156 @@ def test_get_compatibility_text_table():
         assert str(k) in table
     for k in templates.DesignKind:
         assert str(k) in table
+
+
+class TestTool:
+    def test_abc(self):
+        with pytest.raises(NotImplementedError):
+            _Tool.get_kind()
+        with pytest.raises(NotImplementedError):
+            _Tool.get_base_exe_name()
+        with pytest.raises(NotImplementedError):
+            _Tool.get_supported_design_kinds()
+        with pytest.raises(NotImplementedError):
+            _Tool.generate_script(None, None, None)  # type: ignore
+
+    @pytest.mark.parametrize("kind", ToolKind.aslist())
+    def test_settings_wrong_kind(self, kind: ToolKind):
+        # get wrong kind
+        members = list(ToolKind)
+        wrong_index = members.index(kind) + 1
+        if wrong_index >= len(members):
+            wrong_index = 0
+        wrong_kind = members[wrong_index]
+
+        settings = ToolSettings(kind=wrong_kind, bin_dir=Path("/usr/bin"), env={}, extras={})
+
+        with pytest.raises(RuntimeError):
+            _Tool.get_subclass_by_kind(kind)(settings)
+
+    def test_subclass_wrong_kind(self):
+        with pytest.raises(ValueError):
+            _Tool.get_subclass_by_kind("foo")  # type: ignore
+
+
+class _TestGenerateScript:
+    @pytest.fixture
+    def settings(self) -> ToolSettings:
+        raise NotImplementedError
+
+    def _assert_script(self, script: ToolScript):
+        assert len(script.build) > 0
+        for step in script.build:
+            assert step != ""
+        assert len(script.sim) > 0
+        for step in script.sim:
+            assert step != ""
+        assert len(script.waves) > 0
+        for step in script.waves:
+            assert step != ""
+
+    def test_verilog(self, settings: ToolSettings) -> ToolScript:
+        script = generate_script(settings, templates.DesignKind.verilog, ["tb.v"])
+        self._assert_script(script)
+        return script
+
+    def test_sv(self, settings: ToolSettings) -> ToolScript:
+        script = generate_script(settings, templates.DesignKind.sv, ["tb.sv"])
+        self._assert_script(script)
+        return script
+
+    def test_sv_uvm12(self, settings: ToolSettings) -> ToolScript:
+        script = generate_script(settings, templates.DesignKind.sv_uvm12, ["tb_uvm12.sv"])
+        self._assert_script(script)
+        return script
+
+    def test_vhdl(self, settings: ToolSettings) -> ToolScript:
+        script = generate_script(settings, templates.DesignKind.vhdl, ["tb.vhd"])
+        self._assert_script(script)
+        return script
+
+
+class TestGenerateScriptIcarus(_TestGenerateScript):
+    @pytest.fixture
+    def settings(self) -> ToolSettings:
+        return ToolSettings(kind=ToolKind.ICARUS, bin_dir=Path("/usr/bin"), env={}, extras={})
+
+    def test_sv_uvm12(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_sv_uvm12(settings)
+
+    def test_vhdl(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_vhdl(settings)
+
+
+class TestGenerateScriptVerilator(_TestGenerateScript):
+    @pytest.fixture
+    def settings(self) -> ToolSettings:
+        return ToolSettings(kind=ToolKind.VERILATOR, bin_dir=Path("/usr/bin"), env={}, extras={})
+
+    def test_sv_uvm12(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_sv_uvm12(settings)
+
+    def test_vhdl(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_vhdl(settings)
+
+
+class TestGenerateScriptModelsim(_TestGenerateScript):
+    @pytest.fixture
+    def settings(self) -> ToolSettings:
+        return ToolSettings(kind=ToolKind.MODELSIM, bin_dir=Path("/usr/bin"), env={}, extras={})
+
+    def test_sv_uvm12(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_sv_uvm12(settings)
+
+    def test_vhdl(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_vhdl(settings)
+
+
+class TestGenerateScriptXcelium(_TestGenerateScript):
+    @pytest.fixture
+    def settings(self) -> ToolSettings:
+        return ToolSettings(kind=ToolKind.XCELIUM, bin_dir=Path("/usr/bin"), env={}, extras={})
+
+    def test_sv_uvm12(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_sv_uvm12(settings)
+
+    def test_vhdl(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_vhdl(settings)
+
+
+class TestGenerateScriptVcs(_TestGenerateScript):
+    @pytest.fixture
+    def settings(self) -> ToolSettings:
+        return ToolSettings(kind=ToolKind.VCS, bin_dir=Path("/usr/bin"), env={}, extras={})
+
+    def test_vhdl(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_vhdl(settings)
+
+    @pytest.mark.parametrize("gui", ["verdi", "dve"])
+    def test_extras_gui(self, settings: ToolSettings, gui: str):
+        settings.extras["gui"] = gui
+        script = super().test_verilog(settings)
+        for step in script.waves:
+            if gui in step:
+                break
+        else:
+            raise ValueError(f"{gui} executable should be inside waves part of the script")
+
+
+class TestGenerateScriptVivado(_TestGenerateScript):
+    @pytest.fixture
+    def settings(self) -> ToolSettings:
+        return ToolSettings(kind=ToolKind.VIVADO, bin_dir=Path("/usr/bin"), env={}, extras={})
+
+    def test_vhdl(self, settings: ToolSettings):
+        with pytest.raises(ValueError):
+            super().test_vhdl(settings)
